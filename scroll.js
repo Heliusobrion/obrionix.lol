e4adf55d0d2f38186111034000440b6150a48f02
Heliusobrion <107627655+Heliusobrion@users.noreply.github.com>
2022-09-19 05:59:03 +0800

    Create scroll.js
    js

---
+++ scroll.js
@@ -0,0 +1,19 @@
+  },
+    onEnterBack: function () {
+      gsap.fromTo(
+        elem,
+        { y: -100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
